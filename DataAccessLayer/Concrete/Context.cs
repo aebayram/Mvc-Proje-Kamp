9fa9a5c9768d5abfd2f5c8335b8a84f15ccc05a0
Ebru BAYRAM <133230330+aebayram@users.noreply.github.com>
2023-10-10 17:46:24 +0300

    Mvc-Proje-Kampı
    Murat Yücedağ ile 100 saatlik mvc proje kampı

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+	public class Context : DbContext
+	{
+		public DbSet<About> Abouts { get; set; } //DbSet<About> buradaki About sınıf ismi ; Abouts ise; SQL'deki tablonun ismidir. Diğer alanlarda da aynı durum vardır.
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Contact> Contacts { get; set; }
+		public DbSet<Content> Contents { get; set; }
+		public DbSet<Heading> Headings { get; set; }
+		public DbSet<Writer> Writers { get; set; }
+
+
+
+	}
+}
